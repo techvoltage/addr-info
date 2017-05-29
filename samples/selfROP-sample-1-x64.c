6e2dc383217b8ed2113ad9833256c274e13f9104
techvoltage <viv0411.parikh@gmail.com>
2017-05-29 15:01:41 +0530

    Created 64 bit + 32 bit version

---
+++ samples/selfROP-sample-1-x64.c
@@ -0,0 +1,23 @@
+//system() and exit()
+#include "addr-info.h"
+int
+main (void)
+{
+  void (*ptr) (void *);
+  ptr = find_func_address ("system", "libc.so.6");
+  char *bin_sh = "/bin/sh";
+asm (".intel_syntax noprefix;" "mov rdi, %[bin_sh];" "call %[ptr];" ".att_syntax noprefix;":
+: [ptr] "r" (ptr), [bin_sh] "r" (bin_sh):);
+
+//asm (".intel_syntax noprefix;" "mov eax, %0;" "mov ebx, %1;" "push ebx;" "call eax;" ".att_syntax noprefix;":
+//: "r" (ptr), "r" (bin_sh):"eax", "ebx");
+
+  ptr = find_func_address ("exit", "libc.so.6");
+asm (".intel_syntax noprefix;" "mov rax, %0;" "push 0;" "call rax;" ".att_syntax noprefix;":
+: "r" (ptr):"rax");
+
+  ptr (0);
+  printf ("Do not print me\n");
+  return 0;
+
+}
