a26f2200618a7bf0f5e4fac5ebbb5ecd41b3c87f
Sponge <viv0411.parikh@gmail.com>
2017-06-13 09:35:21 +0530

    Makefile for android

--- samples/android/selfROP-sample-1-android.c
+++ samples/android/selfROP-sample-1-android.c
@@ -1,15 +1,18 @@
 //system() and exit()
+#define LIB "/system/lib/libc.so"
 #include "addr-info.h"
 int
 main (void)
 {
   void (*ptr) (void *);
   char *bin_sh = "/bin/sh";
-  ptr = find_func_address ("system", "libc.so");
-asm ("mov r0, %[bin_sh];" "blx %[ptr]":
+  printf("DEBUG msg finding func address");
+  ptr = find_func_address ("system", LIB);
+  printf("DEBUG msg found func address");
+  asm ("mov r0, %[bin_sh];" "blx %[ptr]":
 : [ptr] "r" (ptr),[bin_sh] "r" (bin_sh):);
 
-  ptr = find_func_address ("exit", "libc.so");
+  ptr = find_func_address ("exit", LIB);
 asm ("mov r0, #0;" "blx %[ptr];":
 : [ptr] "r" (ptr):);
 
--- samples/x86/selfROP-sample-1-x86.c
+++ samples/x86/selfROP-sample-1-x86.c
@@ -1,15 +1,16 @@
 //system() and exit()
+#define LIB "libc.so.6"
 #include "addr-info.h"
 int
 main (void)
 {
   void (*ptr) (void *);
-  ptr = find_func_address ("system", "libc.so");
+  ptr = find_func_address ("system", LIB);
   char *bin_sh = "/bin/sh";
 asm (".intel_syntax noprefix;" "push %[bin_sh];" "call %[ptr];" ".att_syntax noprefix;":
 : [ptr] "r" (ptr), [bin_sh] "r" (bin_sh):);
 
-  ptr = find_func_address ("exit", "libc.so");
+  ptr = find_func_address ("exit", LIB);
 asm (".intel_syntax noprefix;" "mov eax, %0;" "push 0;" "call eax;" ".att_syntax noprefix;":
 : "r" (ptr):"eax");
 
